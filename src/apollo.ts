a50b63c81c87655a2dae221493dc5d5b34d03193
LimMinKyo <story1400@naver.com>
2023-09-09 21:41:45 +0900

    ADD: Using the AccessToken

--- src/apollo.ts
+++ src/apollo.ts
@@ -1,12 +1,29 @@
-import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink,
+  makeVar,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import { ACCESS_TOKEN } from "./constants";
 
 const accessToken = localStorage.getItem(ACCESS_TOKEN);
 export const isLoggedInVar = makeVar(Boolean(accessToken));
 export const accessTokenVar = makeVar(accessToken);
 
-export const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: "http://localhost:4000/graphql",
+});
+
+const authLink = setContext((_, { headers }) => ({
+  headers: {
+    ...headers,
+    "x-jwt": accessTokenVar() || "",
+  },
+}));
+
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache({
     typePolicies: {
       Query: {
--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,10 +1,29 @@
-import { isLoggedInVar } from "../apollo";
+import { gql, useQuery } from "@apollo/client";
+import { User } from "../gql/graphql";
+
+const ME_QUERY = gql`
+  query meQuery {
+    me {
+      id
+      email
+      role
+      verified
+    }
+  }
+`;
 
 export default function LoggedInRouter() {
+  const { data, loading, error } = useQuery<{ me: User }>(ME_QUERY);
+  if (!data || loading || error) {
+    return (
+      <div className="h-screen flex justify-center items-center">
+        <span className="font-medium text-xl tracking-wide">Loading...</span>
+      </div>
+    );
+  }
   return (
     <div>
-      <h1>Logged In</h1>
-      <button onClick={() => isLoggedInVar(false)}>Log Out</button>
+      <h1>{data.me.email}</h1>
     </div>
   );
 }
