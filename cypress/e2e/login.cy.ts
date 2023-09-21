cc47399b544fc597add6fdd903252fcafc97f136
LimMinKyo <story1400@naver.com>
2023-09-21 20:28:26 +0900

    ADD: Login Page E2E Tests

---
+++ cypress/e2e/login.cy.ts
@@ -0,0 +1,32 @@
+describe("Log In", () => {
+  const user = cy;
+
+  it("should see login page", () => {
+    user.visit("/").title().should("eq", "Login | Uber Eats");
+  });
+
+  it("can see email / password validation errors", () => {
+    user.visit("/");
+
+    user.findByPlaceholderText(/email/i).type("bad@email");
+    user.findByRole("alert").should("have.text", "Please enter a valid email");
+    user.findByPlaceholderText(/email/i).clear();
+    user.findByRole("alert").should("have.text", "Email is required");
+    user.findByPlaceholderText(/email/i).type("bad@email.com");
+    user
+      .findByPlaceholderText(/password/i)
+      .type("a")
+      .clear();
+    user.findByRole("alert").should("have.text", "Password is required");
+  });
+
+  it("can fill out the form and login", () => {
+    user.visit("/");
+
+    user.findByPlaceholderText(/email/i).type("client@test.com");
+    user.findByPlaceholderText(/password/i).type("12345");
+    user.findByRole("button").should("not.have.class", "pointer-event-none");
+    user.findByRole("button").click();
+    user.window().its("localStorage.access-token").should("be.a", "string");
+  });
+});
