78a656da9f9012e90f2f10f47b13954b46d0ee57
LimMinKyo <story1400@naver.com>
2023-09-21 20:46:39 +0900

    ADD: CreateAccount Page E2E Tests

---
+++ cypress/e2e/create-account.cy.ts
@@ -0,0 +1,47 @@
+describe("Create Account", () => {
+  const user = cy;
+
+  it("should see email / password validation errors", () => {
+    user.visit("/");
+    user.findByText(/create an account/i).click();
+    user.findByPlaceholderText(/email/i).type("non@good");
+    user.findByRole("alert").should("have.text", "Please enter a valid email");
+    user.findByPlaceholderText(/email/i).clear();
+    user.findByRole("alert").should("have.text", "Email is required");
+    user.findByPlaceholderText(/email/i).type("real@email.com");
+    user
+      .findByPlaceholderText(/password/i)
+      .type("a")
+      .clear();
+    user.findByRole("alert").should("have.text", "Password is required");
+  });
+
+  it("should be able to create account and login", () => {
+    user.intercept("http://localhost:4000/graphql", (req) => {
+      const { operationName } = req.body;
+      if (operationName && operationName === "createAccountMutation") {
+        req.reply((res) => {
+          res.send({
+            data: {
+              createAccount: {
+                ok: true,
+                error: null,
+                __typename: "CreateAccountOutput",
+              },
+            },
+          });
+        });
+      }
+    });
+    user.visit("/create-account");
+    user.findByPlaceholderText(/email/i).type("real1@email.com");
+    user.findByPlaceholderText(/password/i).type("12345");
+    user.findByRole("button").click();
+    user.wait(1000);
+    user.title().should("eq", "Login | Uber Eats");
+    user.findByPlaceholderText(/email/i).type("real1@email.com");
+    user.findByPlaceholderText(/password/i).type("12345");
+    user.findByRole("button").click();
+    user.window().its("localStorage.access-token").should("be.a", "string");
+  });
+});
