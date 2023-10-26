4c34ff9ad294d3b02dca6d9a7bcbc10a4e2b5dc7
LimMinKyo <story1400@naver.com>
2023-10-26 20:30:46 +0900

    UPT: Pagination

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -13,33 +13,33 @@ export const Pagination = ({
   onClickNextPage,
   onClickPrevPage,
 }: IProps) => {
-  return (
-    totalPages && (
-      <div className="grid grid-cols-3 text-center max-w-md items-center mx-auto mt-10">
-        {page > 1 ? (
-          <button
-            onClick={onClickPrevPage}
-            className="focus:outline-none font-medium text-2xl"
-          >
-            &larr;
-          </button>
-        ) : (
-          <div></div>
-        )}
-        <span>
-          Page {page} of {totalPages}
-        </span>
-        {page !== totalPages ? (
-          <button
-            onClick={onClickNextPage}
-            className="focus:outline-none font-medium text-2xl"
-          >
-            &rarr;
-          </button>
-        ) : (
-          <div></div>
-        )}
-      </div>
-    )
+  return totalPages ? (
+    <div className="grid grid-cols-3 text-center max-w-md items-center mx-auto mt-10">
+      {page > 1 ? (
+        <button
+          onClick={onClickPrevPage}
+          className="focus:outline-none font-medium text-2xl"
+        >
+          &larr;
+        </button>
+      ) : (
+        <div></div>
+      )}
+      <span>
+        Page {page} of {totalPages}
+      </span>
+      {page !== totalPages ? (
+        <button
+          onClick={onClickNextPage}
+          className="focus:outline-none font-medium text-2xl"
+        >
+          &rarr;
+        </button>
+      ) : (
+        <div></div>
+      )}
+    </div>
+  ) : (
+    <></>
   );
 };
